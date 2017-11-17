553ab91d4a6b4ad729181e0570381d71dc5ab7f8
Jamir Castro Araújo <jamir.c.a@hotmail.com>
2017-11-16 23:00:44 -0200

    new WCF Example

---
+++ WCFService/WCFService/DataService.svc.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace WCFService
+{
+    public class DataService : IDataService
+    {
+        public Data GetData()
+        {
+            return new Data("TEXT");
+        }
+    }
+
+    [DataContract]
+    public class Data
+    {
+        [DataMember]
+        public string Text { get; set; }
+
+        public Data(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ WCFService/WCFService/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+
+namespace WCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IDataService" in both code and config file together.
+    [ServiceContract]
+    public interface IDataService
+    {
+        [OperationContract]
+        Data GetData();
+    }
+}
