e5c8bf8e6e16f20ccf018d8bc3707be8efa46511
Jamir Castro Araújo <jamir.c.a@hotmail.com>
2017-12-27 22:58:54 -0200

    layout do time out para chamadas síncronas finalizando

--- DesktopExamples/DesktopExamples/OperationsDataDetails.xaml.cs
+++ DesktopExamples/DesktopExamples/OperationsDataDetails.xaml.cs
@@ -30,7 +30,7 @@ namespace DesktopExamples
                     .OrderBy(p => p.StartTime);
 
                 var threadTimeline = new TimelineTray();
-                
+
                 threadTimeline.MinDateTime = requests.Min(p => p.StartTime);
                 threadTimeline.MaxDateTime = requests.Max(p => p.EndTime);
 
@@ -38,14 +38,14 @@ namespace DesktopExamples
                 threadTimeline.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Auto });
 
                 var milliseconds10Band = new TimelineBand();
-                milliseconds10Band.ItemSourceType = "milliseconds10";
+                milliseconds10Band.ItemSourceType = "milliseconds100";
                 milliseconds10Band.IsMainBand = true;
                 threadTimeline.Children.Add(milliseconds10Band);
 
                 var secondsBand = new TimelineBand();
                 secondsBand.ItemSourceType = "seconds";
                 threadTimeline.Children.Add(secondsBand);
-                
+
 
                 foreach (var request in requests)
                 {
@@ -62,11 +62,6 @@ namespace DesktopExamples
                 threadTimeline.Run();
                 ThreadItemsControl.Items.Add(threadTimeline);
             }
-
-            //timeline.TimelineEvents.Add(new TimelineEvent { Title = "Teste", StartDate = DateTime.Now.AddMilliseconds(50), EndDate = DateTime.Now });
-            //timeline.MaxDateTime = DateTime.Now.AddSeconds(1);
-            //timeline.MinDateTime = DateTime.Now.AddSeconds(-1);
-            //timeline.Run();
         }
 
         private string GetRequestDescription(SyncRequestData request)
@@ -74,7 +69,9 @@ namespace DesktopExamples
             return $"RequestId: {request.RequestId}\n" +
                 $"Duration: {request.Duration}\n" +
                 $"ParamValue: {request.ParamValue}\n" +
-                $"ThreadId {request.ThreadId}";
+                $"ThreadId: {request.ThreadId}\n" +
+                $"Start: {request.StartTime}\n" +
+                $"End: {request.EndTime}";
         }
     }
 }
